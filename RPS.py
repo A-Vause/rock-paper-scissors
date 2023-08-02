42db642e6b51c09342a73f5b5193d5f8c425458c
A-Vause <adam.p.vause@gmail.com>
2023-08-02 17:43:02 +0100

    bug fix for invalid number

--- RPS.py
+++ RPS.py
@@ -29,25 +29,30 @@ scissors = '''
 
 game_images = [rock, paper, scissors]
 choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for scissors\n"))
-print(game_images[choice])
-
-print("Computer chose:")
-comp_choice = random.randint(0, 2)
-print(game_images[comp_choice])
-
-
-if choice == 0 and comp_choice == 2:
-    print("You win!")
-elif choice == 1 and comp_choice == 0:
-    print("You win!")
-elif choice == 2 and comp_choice == 0:
-    print("You lose!")
-elif choice == 2 and comp_choice == 1:
-    print("You win!")
-elif comp_choice > choice:
-    print("You lose!")
-elif choice == comp_choice:
-    print("It's a draw")
+if choice >= 3 or choice < 0:
+    print("Invalid choice. You lose!")
 else:
-    print("Invalid input. You lose")
+
+    print(game_images[choice])
+
+
+    print("Computer chose:")
+    comp_choice = random.randint(0, 2)
+    print(game_images[comp_choice])
+
+
+
+    if choice == 0 and comp_choice == 2:
+        print("You win!")
+    elif choice == 1 and comp_choice == 0:
+        print("You win!")
+    elif choice == 2 and comp_choice == 0:
+        print("You lose!")
+    elif choice == 2 and comp_choice == 1:
+        print("You win!")
+    elif comp_choice > choice:
+        print("You lose!")
+    elif choice == comp_choice:
+        print("It's a draw")
+
 
